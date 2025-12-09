06b21dd79a1d1012b082ea560e5b863483061e4c
Victoria <leonardv@uwm.edu>
2025-12-09 11:10:51 -0600

    updated models and admin dashboard

--- quiz_taker/admin.py
+++ quiz_taker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+models_to_register = [Answers, Questions, Quizzes, StudentAttempts,
+                      StudentResponses, Users, Roles, QuizCategories,
+                      AuditLogs]
+admin.site.register(models_to_register)
